55c5f191b5fb1dd29bda3b08380bddf2342b78a5
Szaloki <szalokipeterkrisztian@gmail.com>
2025-02-11 22:38:51 +0100

    News update

--- backend_Uni/app/Http/Controllers/NewsController.php
+++ backend_Uni/app/Http/Controllers/NewsController.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\News;
+use App\Http\Requests\StoreNewsRequest;
+use App\Http\Requests\UpdateNewsRequest;
 
 class NewsController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreNewsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(News $news)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateNewsRequest $request, News $news)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(News $news)
+    {
+        //
+    }
 }
