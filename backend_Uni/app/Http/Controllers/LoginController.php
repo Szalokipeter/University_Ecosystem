d2a9f5951431b2701e8af8f9b34d7c87caf3cb22
Szaloki <szalokipeterkrisztian@gmail.com>
2025-02-11 20:42:22 +0100

    Routes Implemented

--- backend_Uni/app/Http/Controllers/LoginController.php
+++ backend_Uni/app/Http/Controllers/LoginController.php
@@ -3,8 +3,44 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class LoginController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required',
+            'password' => 'required'
+        ]);
+        if(Auth::attempt($validated)) {
+            $request->session()->regenerate();
+            return response()->json([
+                'message' => 'Login successful'
+            ], 200);
+        }
+        return response()->json([
+            'message' => 'Login failed'
+        ], 401);
+    }
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+        $validated['password'] = bcrypt($validated['password']);
+        try {
+            DB::table('users')->insert($validated);
+            return response()->json([
+                'message' => 'Registration successful'
+            ], 201);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Registration failed'
+            ], 400);
+        }
+    }
 }
--- backend_Uni/routes/api.php
+++ backend_Uni/routes/api.php
@@ -1,12 +1,17 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CalendarController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\NewsController;
 use App\Http\Controllers\PublicCalendarController;
 use App\Http\Controllers\TodoController;
 use Illuminate\Session\Middleware\StartSession;
-use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 
 Route::middleware([StartSession::class])->group(function () {
@@ -16,12 +21,13 @@ Route::middleware([StartSession::class])->group(function () {
 
 
 Route::middleware('auth:sanctum')->group(function(){
-    Route::apiResource('personalCalendar/{id}', CalendarController::class); // ez a route minden tag számára elérhető (controller ellenőrzi hogy saját maga vagy admin)
-    Route::apiResource('personalTodos/{id}', TodoController::class); // ez a route minden tag számára elérhető (controller ellenőrzi hogy saját maga vagy admin)
+    Route::apiResource('users/{user}/personalCalendar', CalendarController::class); // ez a route minden tag számára elérhető (controller ellenőrzi hogy saját maga vagy admin)
+    Route::apiResource('users/{user}/personalTodos', TodoController::class); // ez a route minden tag számára elérhető (controller ellenőrzi hogy saját maga vagy admin)
     Route::apiResource('uniCalendar', PublicCalendarController::class, ['except' => 'index']); // ez a route vegyesen érhető el, a get minden tag számára elérhető, a post,put,delete az csak Admin vagy oktató számára érhető el.
 
     Route::apiResource('news', NewsController::class, ['except' => 'index']); // ez a route vegyesen érhető el, a get minden tag számára elérhető, a post,put,delete az csak Admin vagy oktató számára érhető el.
+
 });
 
-Route::get("uniCalendar", PublicCalendarController::class, "index");
-Route::get("news", NewsController::class, "index");
+Route::get("uniCalendar", [PublicCalendarController::class, "index"]);
+Route::get("news", [NewsController::class, "index"]);
