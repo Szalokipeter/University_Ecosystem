0664a4b882fc4a1715d76d0ebcc1f4d762e04440
Szaloki Peter <szaloki.peter.krisztian@students.jedlik.eu>
2025-02-11 13:27:40 +0100

    Backend Setup

---
+++ backend_Uni/routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\CalendarController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\PublicCalendarController;
+use App\Http\Controllers\TodoController;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Support\Facades\Route;
+
+
+Route::middleware([StartSession::class])->group(function () {
+    Route::post('login', [LoginController::class, 'login']); //ez a route bárki számára használható
+    Route::post('register', [LoginController::class, 'register']); //ez a route csak admin-ok számára használható
+});
+
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::apiResource('personalCalendar/{id}', CalendarController::class); // ez a route minden tag számára elérhető (controller ellenőrzi hogy saját maga vagy admin)
+    Route::apiResource('personalTodos/{id}', TodoController::class); // ez a route minden tag számára elérhető (controller ellenőrzi hogy saját maga vagy admin)
+    Route::apiResource('uniCalendar', PublicCalendarController::class, ['except' => 'index']); // ez a route vegyesen érhető el, a get minden tag számára elérhető, a post,put,delete az csak Admin vagy oktató számára érhető el.
+
+    Route::apiResource('news', NewsController::class, ['except' => 'index']); // ez a route vegyesen érhető el, a get minden tag számára elérhető, a post,put,delete az csak Admin vagy oktató számára érhető el.
+});
+
+Route::get("uniCalendar", PublicCalendarController::class, "index");
+Route::get("news", NewsController::class, "index");
