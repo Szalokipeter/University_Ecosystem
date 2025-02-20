a6b9f00c0f97248aca647c4561b7a12be93bc3b8
Szaloki Peter <szaloki.peter.krisztian@students.jedlik.eu>
2025-02-20 13:28:47 +0100

    underway_todos

--- backend_Uni/app/Http/Controllers/TodoController.php
+++ backend_Uni/app/Http/Controllers/TodoController.php
@@ -13,25 +13,35 @@ class TodoController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Uniuser $User)
     {
-        /** @var UniUser $user */
+        /** @var UniUser $validateduser */
         $validateduser = Auth::user();
-        return $validateduser->todos;
+        if(!$validateduser->isAdmin() && $validateduser->id !== $User->id){
+            return response()->json(['message' => 'Unauthorized'], 403);
+        }
+
+        $todos = Todo::where('uni_user_id', $User->id)->get();
+        return response()->json($todos);
 
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreTodoRequest $request)
+    public function store(UniUser $User, StoreTodoRequest $request)
     {
-        /** @var UniUser $user */
+        /** @var UniUser $validateduser */
         $validateduser = Auth::user();
+        if(!$validateduser->isAdmin() && $validateduser->id != $User->id){
+            return response()->json(['message' => 'Unauthorized'], 403);
+        }
+
         try {
             $data = $request->validated();
             $data['status'] = "todo";
-            $data['uni_user_id'] = $validateduser->id;
+            $data['uni_user_id'] = $User->id;
+
             // dd($data);
             $todo = Todo::create($data);
             return response()->json($todo, 201);
@@ -44,14 +54,16 @@ class TodoController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show($id)
+    public function show(UniUser $User, $todo)
     {
-        $todo = Todo::find($id);
-        /** @var UniUser $user */
+        // $todo = Todo::find($todo->id);
+        /** @var UniUser $validateduser */
+        dd([$User, $todo]);
         $validateduser = Auth::user();
-        // if ($validateduser->id !== $todo->uni_user_id || !$validateduser->isAdmin()) {
-        //     return response()->json(['message' => 'Unauthorized'], 403);
-        // }
+         if ($validateduser->id !== $User->id && !$validateduser->isAdmin()) {
+             return response()->json(['message' => 'Unauthorized'], 403);
+         }
+
         return response()->json($todo);
 
     }
@@ -59,16 +71,38 @@ class TodoController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateTodoRequest $request, Todo $todo)
+    public function update(UniUser $User, Todo $todo, UpdateTodoRequest $request )
     {
-        //
+        /** @var UniUser $validateduser */
+        $validateduser = Auth::user();
+        if ($validateduser->id !== $User->id && !$validateduser->isAdmin()) {
+            return response()->json(['message' => 'Unauthorized'], 403);
+        }
+        try {
+            //code...
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+        if(!$todo->update($request->validated())){
+            return response()->json(["message"=>"Todo could not be updated."], 500);
+        }
+        return response()->json($todo);
+
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Todo $todo)
+    public function destroy(UniUser $User, Todo $todo)
     {
-        //
+        /** @var UniUser $validateduser */
+        $validateduser = Auth::user();
+        if ($validateduser->id !== $todo->uni_user_id && !$validateduser->isAdmin()) {
+            return response()->json(['message' => 'Unauthorized'], 403);
+        }
+        if(!$todo->delete()){
+            return response()->json(["message"=>"Todo could not be deleted."], 500);
+        }
+        return response()->json(["message"=>"Todo was deleted."]);
     }
 }
--- backend_Uni/app/Http/Requests/UpdateTodoRequest.php
+++ backend_Uni/app/Http/Requests/UpdateTodoRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateTodoRequest extends FormRequest
 {
@@ -22,7 +23,9 @@ class UpdateTodoRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "title"=>['required', "string", "max:255"],
+            "body" => ["required", "string"],
+            "status" => ["required", "string", Rule::in(["todo", "done", "in-progress"])],
         ];
     }
 }
