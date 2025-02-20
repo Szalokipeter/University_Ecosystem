0baf38f990abc581081851b3d78bb2f2033770a3
Szaloki Peter <szaloki.peter.krisztian@students.jedlik.eu>
2025-02-20 14:44:45 +0100

    Started working on personalCalendar, get all works

--- backend_Uni/app/Http/Controllers/CalendarController.php
+++ backend_Uni/app/Http/Controllers/CalendarController.php
@@ -5,15 +5,25 @@ namespace App\Http\Controllers;
 use App\Models\Calendar;
 use App\Http\Requests\StoreCalendarRequest;
 use App\Http\Requests\UpdateCalendarRequest;
+use App\Models\UniUser;
+use Illuminate\Support\Facades\Auth;
 
 class CalendarController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(UniUser $user)
     {
-        //
+        /** @var UniUser $validateduser */
+        $validateduser = Auth::user();
+        if(!$validateduser->isAdmin() && $validateduser->id !== $user->id){
+            return response()->json(['message' => 'Unauthorized'], 403);
+        }
+
+        $events = Calendar::where('uni_user_id', $user->id)->get();
+        return response()->json($events);
+
     }
 
     /**
