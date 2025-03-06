50f6e877b19f6b8d2775e1833d68214683f1ff32
Szaloki <szalokipeterkrisztian@gmail.com>
2025-03-06 20:29:12 +0100

    .bat files for easier usage during development

---
+++ install.bat
@@ -0,0 +1,22 @@
+@echo off
+title DO NOT CLOSE THIS WINDOW
+echo .
+echo Installing Backend and Test Frontend
+echo .
+
+cd /d "%~dp0"
+cd backend_uni
+echo Backend Directory: %cd%
+echo .
+
+echo Installing Backend in 3 seconds...
+timeout /t 3 >nul
+powershell "# Run as administrator... Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://php.new/install/windows/8.4'))"
+echo C:\Users\%USERNAME%\.config\herd-lite\bin\php.ini
+set "PHPINI_PATH=C:\Users\%USERNAME%\.config\herd-lite\bin\php.ini"
+powershell -Command "(Get-Content -LiteralPath '%PHPINI_PATH%') -replace 'variables_order = \"EGPCS\"', 'variables_order = \"GPCS\"' | Set-Content -LiteralPath '%PHPINI_PATH%'"
+start "npm Install" cmd /c "npm install && timeout /t 1 && exit"
+start "Composer Install" cmd /c "composer install && timeout /t 1 && exit"
+@REM start "Frontend npm Install" cmd /c "cd .. && cd laravel-echo-angular && npm install && timeout /t 1 && exit"
+
+exit
---
+++ start.bat
@@ -0,0 +1,8 @@
+@echo off
+title Staring Backend and Test Frontend
+
+start /min cmd /k "cd backend_uni && php artisan serve"
+start /min cmd /k "cd backend_uni && php artisan reverb:start"
+start /min cmd /k "cd backend_uni && php artisan queue:work"
+@REM start /min cmd /k "cd laravel-echo-angular && ng serve"
+exit
