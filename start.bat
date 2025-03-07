8650660d0009554f56c9962b47a8b702ab09b60f
Szaloki Peter <szaloki.peter.krisztian@students.jedlik.eu>
2025-03-07 10:53:00 +0100

    forgot to add VSCode opening into .bat file

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
-title Staring Backend and Test Frontend
+start /min cmd /c "code . && exit"
+title Staring Backend
 cd backend_uni
 npx concurrently -k "php artisan serve" "php artisan reverb:start" "php artisan queue:work"
-pause >nul
