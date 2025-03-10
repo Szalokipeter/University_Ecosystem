44b1f0ac7a31e9e45faaf38f2f6da9d72be62ba2
Szaloki <szalokipeterkrisztian@gmail.com>
2025-03-10 23:38:42 +0100

    ShouldBrotcastNow implementation (replaced php artisan queue:work with it)

--- start.bat
+++ start.bat
@@ -2,4 +2,4 @@
 start /min cmd /c "code . && exit"
 title Staring Backend
 cd backend_uni
-npx concurrently -k "php artisan serve" "php artisan reverb:start" "php artisan queue:work"
+npx concurrently -k "php artisan serve" "php artisan reverb:start"
